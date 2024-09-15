2427203b603c2aeda8b5e270649afe04cef9f7bb
SofyaLev <sofya_levchuk@bk.ru>
2024-09-15 16:35:59 +0300

    Version 1.0

--- lab0/Task 2/task2.py
+++ lab0/Task 2/task2.py
@@ -1,18 +1,24 @@
+
+def calc_fib(n):
+    if n == 0:
+        file.write('0')
+    elif n == 1:
+        file.write('1')
+    else:
+        l = [0, 1]
+        for i in range(1, n):
+            l.append(l[i-1] + l[i])
+    return l[n]
+
+
 with open('input.txt', 'r') as file:
     n = int(file.readline())
 file.close()
 
 if 0 <= n <= 45:
+    fib = calc_fib(n)
     with open('output.txt', 'w') as file:
-        if n == 0:
-            file.write('0')
-        elif n == 1:
-            file.write('1')
-        else:
-            l = [0, 1]
-            for i in range(1, n):
-                l.append(l[i-1] + l[i])
-            file.write(str(l[n]))
+        file.write(str(fib))
     file.close()
 else:
-    print('Числа не соответствуют условию')
+    print('Число не соответсвует условию')
--- lab0/Task 4/task2.py
+++ lab0/Task 4/task2.py
@@ -1,11 +1,7 @@
 import time
 start = time.perf_counter()
 
-with open('input.txt', 'r') as file:
-    n = int(file.readline())
-file.close()
-
-with open('output.txt', 'w') as file:
+def calc_fib(n):
     if n == 0:
         file.write('0')
     elif n == 1:
@@ -14,6 +10,18 @@ with open('output.txt', 'w') as file:
         l = [0, 1]
         for i in range(1, n):
             l.append(l[i-1] + l[i])
-        file.write(str(l[n]))
+    return l[n]
+
+
+with open('input.txt', 'r') as file:
+    n = int(file.readline())
 file.close()
+
+if 0 <= n <= 45:
+    fib = calc_fib(n)
+    with open('output.txt', 'w') as file:
+        file.write(str(fib))
+    file.close()
+else:
+    print('Число не соответсвует условию')
 print(time.perf_counter() - start)
