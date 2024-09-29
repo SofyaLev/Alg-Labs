0208407ce9f916444d93d927b40bb5b3217fa22f
SofyaLev <sofya_levchuk@bk.ru>
2024-09-29 22:08:42 +0300

    added files

--- lab1/task6/src/task6.py
+++ lab1/task6/src/task6.py
@@ -6,14 +6,23 @@ def bubble_sort(n, numbers):
     return numbers
 
 
-# with open('input.txt', 'r') as file:
-#     n = int(file.readline())
-#     numbers = list(map(int, file.readline().split()))
-# file.close()
-#
-# if 1 <= n <= 10**3:
-#     with open('output.txt', 'w') as file:
-#         file.write(' '.join(map(str, bubble_sort(n, numbers))))
-#     file.close()
-# else:
-#     print('веденные данные не соответствуют условию')
+def check(numbers):
+    for i in range(len(numbers)-1):
+        if not(numbers[i] <= numbers[i+1]):
+            return False
+    return True
+
+
+with open('input.txt', 'r') as file:
+    n = int(file.readline())
+    numbers = list(map(int, file.readline().split()))
+file.close()
+
+if 1 <= n <= 10**3:
+    with open('output.txt', 'w') as file:
+        file.write(' '.join(map(str, bubble_sort(n, numbers))))
+    file.close()
+else:
+    print('веденные данные не соответствуют условию')
+
+print(check(bubble_sort(n, numbers)))
