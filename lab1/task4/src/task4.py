62e5ebb0e41a9529b56fc474f717e8758bf41b71
SofyaLev <sofya_levchuk@bk.ru>
2024-09-30 11:26:41 +0300

    added files

--- lab1/task4/src/task4.py
+++ lab1/task4/src/task4.py
@@ -12,10 +12,14 @@ def linear_search(A, V):
         return counter, result
 
 
-# A = list(map(int, input().split()))
-# V = int(input())
+# with open('input.txt', 'r') as file:
+#     A = list(map(int, file.readline().split()))
+#     V = int(file.readline())
+# file.close()
 #
 # if (0 <= len(A) <= 10**3) and (-10**3 <= V <= 10**3):
-#     print(*linear_search(A, V), sep=', ')
+#     with open('output.txt', 'w') as file:
+#         file.write(', '.join(map(str, linear_search(A, V))))
+#     file.close()
 # else:
-#     print('Введенные данные не соответсвуют условию')
+#     print('Введенные данные не соответствуют условию')
--- lab1/task6/src/task6.py
+++ lab1/task6/src/task6.py
@@ -13,16 +13,16 @@ def check(numbers):
     return True
 
 
-with open('input.txt', 'r') as file:
-    n = int(file.readline())
-    numbers = list(map(int, file.readline().split()))
-file.close()
-
-if 1 <= n <= 10**3:
-    with open('output.txt', 'w') as file:
-        file.write(' '.join(map(str, bubble_sort(n, numbers))))
-    file.close()
-else:
-    print('веденные данные не соответствуют условию')
-
-print(check(bubble_sort(n, numbers)))
+# with open('input.txt', 'r') as file:
+#     n = int(file.readline())
+#     numbers = list(map(int, file.readline().split()))
+# file.close()
+#
+# if 1 <= n <= 10**3:
+#     with open('output.txt', 'w') as file:
+#         file.write(' '.join(map(str, bubble_sort(n, numbers))))
+#     file.close()
+# else:
+#     print('веденные данные не соответствуют условию')
+#
+# print(check(bubble_sort(n, numbers)))
